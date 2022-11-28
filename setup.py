b87a38e530d962359c2a0a5960ac9b8366dea74c
Josh Nicholas <hi@thambi.li>
2022-11-28 14:38:57 +1100

    First functions

--- setup.py
+++ setup.py
@@ -1,3 +1,26 @@
-from setuptools import setup
+import setuptools
 
-setup()
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "sudulunu",
+    version = "0.0.2",
+    author = "Josh Nicholas",
+    author_email = "josh@thambi.li",
+    description = "A package with some helper scripts",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "package URL",
+    project_urls = {
+        "Bug Tracker": "package issues URL",
+    },
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir = {"": "src"},
+    packages = setuptools.find_packages(where="src"),
+    python_requires = ">=3.9"
+)
