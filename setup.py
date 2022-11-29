1f4fce9bce370e992b76984c7cba35ad247c4306
Josh Nicholas <hi@thambi.li>
2022-11-29 11:22:32 +1100

    More scripts

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding = "utf-8") as fh:
 
 setuptools.setup(
     name = "sudulunu",
-    version = "0.0.2",
+    version = "0.0.3",
     author = "Josh Nicholas",
     author_email = "josh@thambi.li",
     description = "A package with some helper scripts",
--- src/sudulunu/helpers.py
+++ src/sudulunu/helpers.py
@@ -2,7 +2,8 @@ import os
 import pandas as pd
 import time 
 import random 
-
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
 
 def pp(frame):
   inter = frame.copy()
@@ -69,6 +70,57 @@ def rand_delay(num):
   time.sleep(rando)
 
 
+def return_similar(list_one, list_two, threshold=90, limit=5):
 
+	from fuzzywuzzy import fuzz
+	from fuzzywuzzy import process
+	dicto = {}
+
+	sec = list_two.copy()
+
+	inter = [x for x in list_one if x not in sec]
+
+	for word in inter:
+
+		result = process.extract(word, sec, limit=limit)
+		result = [x for x in result if (x[1] >= threshold) and (x[0] != word)] 
+		# print(word)  
+		# print(result) 
+
+		if len(result) > 0:
+				result = result[0]
+				dicto[word] = result[0]
+
+	return dicto 
+
+
+def matcher(pattern, stringo, return_num):
+  import re
+  searcho = re.search(pattern, stringo).group(return_num)
+  print(searcho)
+
+
+def return_similar(list_one, list_two, threshold=90, limit=5):
+
+	from fuzzywuzzy import fuzz
+	from fuzzywuzzy import process
+	dicto = {}
+
+	sec = list_two.copy()
+
+	inter = [x for x in list_one if x not in sec]
+
+	for word in inter:
+
+		result = process.extract(word, sec, limit=limit)
+		result = [x for x in result if (x[1] >= threshold) and (x[0] != word)] 
+		# print(word)  
+		# print(result) 
+
+		if len(result) > 0:
+				result = result[0]
+				dicto[word] = result[0]
+
+	return dicto 
 
 
